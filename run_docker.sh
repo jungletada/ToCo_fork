512d6f111dbf46e8f22065ca8ce999cde209e3f2
jungletada <1002955176@qq.com>
2024-10-26 17:54:58 +0900

    docker

--- run_docker.sh
+++ run_docker.sh
@@ -5,4 +5,4 @@ docker run -it --network=host --ipc=host \
 -v ~/code/ToCo_fork_CVPR23:/workspace/TOCO \
 -v ~/data/VOCdevkit:/workspace/VOCdevkit \
 -v ~/data/MSCOCO:/workspace/MSCOCO \
--v ~/data/MSCOCO:/coco2014 toco:latest /bin/bash
+-v ~/data/MSCOCO/MaskSets:/coco2014 toco:latest /bin/bash
